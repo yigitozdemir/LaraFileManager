d7a0f46e629985b4b9fd66810f6cc7980615fc1c
Yiğit Özdemir <yigitozdemir@yandex.com>
2023-08-23 12:37:06 +0300

    models ready

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory()->create(
+            [
+                'name' => 'Yigit Ozdemir',
+                'email' => 'yigit@yigitnot.com',
+                'password'=> bcrypt('123456')
+            ]
+        );
+    }
+}
